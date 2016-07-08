178650274cf6d4b851fef66b1ed6dcbfcd74a300
Ryan Palermo <stix250@gmail.com>
2016-07-08 12:59:55 -0400

    Done.

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -9,8 +9,24 @@ class RecipesController < ApplicationController
 
   def new
     @recipe = Recipe.new
+    @recipe.ingredients.build
+    @recipe.ingredients.build
   end
 
   def create
+    @recipe = Recipe.new(recipe_params)
+    ingredients_params[:ingredients_attributes].values.each do |param|
+      @recipe.ingredients << Ingredient.new(param) unless @recipe.ingredients.include?(Ingredient.new(param))
+    end
+    @recipe.save
+    redirect_to @recipe
+  end
+
+  def recipe_params
+    params.require(:recipe).permit(:title)
+  end
+
+  def ingredients_params
+    params.require(:recipe).permit(:ingredients_attributes => [:name, :quantity])
   end
 end
